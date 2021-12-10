479d6c4fa78a84e8f6450c6b2c1ea601cb8676af
frogerro <romain.froger@etu.utc.fr>
2021-12-10 20:35:11 +0100

    tostring type

--- Splendor/src/type/type.cpp
+++ Splendor/src/type/type.cpp
@@ -2,4 +2,17 @@
 
 namespace Splendor{
     std::initializer_list<Type> Types = {Type::nobles, Type::cite, Type::un, Type::deux, Type::trois};
+
+    std::string toString(Type c) {
+		switch (c) {
+		case Type::nobles: return "Nobles";
+		case Type::cite: return "Cite";
+		case Type::un: return "I";
+		case Type::deux: return "II";
+		case Type::trois: return "III";
+
+		default: throw SplendorException("Couleur inconnue");
+		}
+	}
+
 }
--- Splendor/src/type/type.h
+++ Splendor/src/type/type.h
@@ -2,16 +2,18 @@
 #define TYPE_H
 
 #include <iostream>
-#include <string.h>
 #include <cstdlib>
 #include <stdlib.h>
 #include <initializer_list>
 #include <array>
+#include <string>
+
+#include "../splendorException/splendorexception.h"
 
 namespace Splendor{
 
     //enum class Type {I = 1, II = 2, III = 3, Noble = 4};
-    enum class Type {nobles, cite, un=1, deux=2, trois=3 };
+    enum class Type {nobles=1, cite=2, un=3, deux=4, trois=5 };
     extern std::initializer_list<Type> Types;
     
 }
