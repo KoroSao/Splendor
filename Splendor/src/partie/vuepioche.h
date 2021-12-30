a3b3f8738c1faab60bd7139d34405edc58f092b9
Francois Guichard <francois.guichard@etu.utc.fr>
2021-12-30 19:18:56 +0100

    creation class VuePioche

---
+++ Splendor/src/partie/vuepioche.h
@@ -0,0 +1,38 @@
+#ifndef VUEPIOCHE_H
+#define VUEPIOCHE_H
+
+#include <QWidget>
+#include <QPushButton>
+#include <QGroupBox>
+#include <vector>
+#include <set>
+#include "../controleur/controleur.h"
+#include <QPen>
+#include <QBrush>
+#include <QPushButton>
+
+
+class VuePioche : public QPushButton
+{
+    Q_OBJECT
+public:
+    VuePioche(QWidget* parent = nullptr);
+    VuePioche(const Splendor::Pioche& p, QWidget *parent = nullptr);
+    const Splendor::Pioche& getPioche() const { return *pioche; }
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+private:
+    const Splendor::Pioche* const pioche;
+    QPen pen;
+    QBrush brush;
+signals:
+    // quand la vue de pioche est cliquée, elle émet un signal en transmettant son adresse
+    void piocheClicked(VuePioche*);
+public slots:
+private slots:
+    void clickedEvent() { emit piocheClicked(this); }
+};
+
+
+#endif // VUEPIOCHE_H
